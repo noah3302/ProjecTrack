818a0ab3641a68a376406953c6be75b8e908861d
Markus Schwarzkopf <ms543@hdm-stuttgart.de>
2023-11-13 19:35:52 +0100

    bo comment

---
+++ backend/server/bo/Comment.py
@@ -0,0 +1,55 @@
+from backend.server.bo.BuisnessObject import BusinessObject
+
+class Comment(BusinessObject):
+    def __init__(self):
+        super().__init__()
+        self._comment_id = 0
+        self._comment = ""
+        self._erstellungsdatum = ""
+        self._user_id = 0
+        self._task_id = 0
+
+    def get_comment_id(self):
+        return self._comment_id
+
+    def set_comment_id(self, comment_id):
+        self._comment_id = comment_id
+
+    def get_comment(self):
+        return self._comment
+
+    def set_comment(self, comment):
+        self._comment = comment
+
+    def get_erstellungsdatum(self):
+        return self._erstellungsdatum
+
+    def set_erstellungsdatum(self, erstellungsdatum):
+        self._erstellungsdatum = erstellungsdatum
+
+    def get_user_id(self):
+        return self._user_id
+
+    def set_user_id(self, user_id):
+        self._user_id = user_id
+
+    def get_task_id(self):
+        return self._task_id
+
+    def set_task_id(self, task_id):
+        self._task_id = task_id
+
+    def __str__(self):
+        return "comment_id: {}\ncomment: {}\nerstellungsdatum: {}\nuser_id: {}\ntask_id: {}\n".format(
+            self._comment_id, self._comment, self._erstellungsdatum, self._user_id, self._task_id)
+
+    @staticmethod
+    def from_dict(dictio=dict()):
+        obj = Comment()
+        obj.set_comment_id(dictio["comment_id"])
+        obj.set_comment(dictio["comment"])
+        obj.set_erstellungsdatum(dictio["erstellungsdatum"])
+        obj.set_user_id(dictio["user_id"])
+        obj.set_task_id(dictio["task_id"])
+
+        return obj
