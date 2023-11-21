f5c690f7094e3700a75b8ff0b7bc9d9f9bcafa6c
Theresa Kottmann <tk154@hdm-stuttgart.de>
2023-11-21 15:32:35 +0100

    Update Tasks.js
    Task hinzufügen angelegt

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,9 +1,52 @@
-//import React, { useEffect, useState } from 'react';
-//import { apiget } from "../API/Api";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
 
+export default function Tasks() {
+  const [selectedDate, setSelectedDueDate] = useState("");
 
-//folgt
+  const handleDueDate = (event) => {
+    setSelectedDueDate(event.target.value);
+  };
 
+  return (
+    <Box
+      sx={{ width: "250px", "& .MuiTextField-root": { marginTop: "20px" } }} //Der Abstand von den Textfeldern wird hier angegeben.
+      
+    >
+      <div>
+        <TextField                                 // Titel einfügen
+          required
+          id="outlined-required-title"
+          label="Titel"                          
+          placeholder="neuer Händler"
+        />
+        <TextField                                // Beschreibung einfügen
+          required
+          id="outlined-required-description"
+          label="Beschreibung"
+          placeholder="Suche einen neuen Händler."
+        />
 
-//export default Tasks;
+        <TextField                                 // Mitglied einer Task zuweisen
+          required
+          id="outlined-required-description"
+          label="einem Mitglied zuweisen"
+          placeholder="Max009"
+        />
 
+        <TextField
+          id="outlined-date"
+          label="Due Date"                        // Fälligkeitsdatum auswählen
+          type="date"
+          value={selectedDate}
+          onChange={handleDueDate}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </div>
+    </Box>
+  );
+}
