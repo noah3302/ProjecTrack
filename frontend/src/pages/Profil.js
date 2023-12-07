cc91c22034c9ce376e17c461a8015889ce43648b
Markus Schwarzkopf <ms543@hdm-stuttgart.de>
2023-12-07 21:24:01 +0100

    edit profil

--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,106 +1,180 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
-import { Card, Typography, Button, Box } from '@mui/material';
-import { apiput, apidelete, apiget } from '../API/Api';
-import { UserAuth } from "../Context/Authcontext";
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import { apiget, apiput, apidelete, apipost } from '../API/Api';
+import { UserAuth } from '../Context/Authcontext';
 
 export default function Profil() {
-    const [vorname, setVorname] = React.useState('');
-    const [nachname, setNachname] = React.useState('');
-    const [nickname, setNickname] = React.useState('');
-    const [isEditMode, setIsEditMode] = React.useState(false);
+  const { user, setUser } = UserAuth();
 
-    const { user } = UserAuth(); // Assuming UserAuth provides the user object
+  console.log("user", user)
 
-    const input = {
-        margin: "1rem",
-        backgroundColor: "withe",
-        width: "18rem"
-    };
-    
-    console.log("user", user)
+  const [vorname, setVorname] = useState('');
+  const [nachname, setNachname] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [helper, setHelper] = useState('');
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [existingNicknames, setExistingNicknames] = useState([]);
 
-    React.useEffect(() => {
-        // Fetch user data for editing if user ID is available
-        const fetchUserData = async () => {
-            try {
-                const userData = await apiget(`users/${user.google_id}`);
-                if (userData) {
-                    setVorname(userData.vorname || '');
-                    setNachname(userData.nachname || '');
-                    setNickname(userData.nickname || '');
-                    setIsEditMode(true);
-                }
-            } catch (error) {
-                console.error("Fehler beim Laden des Profils:", error);
-            }
-        };
+  useEffect(() => {
+    fetchUserData();
+    fetchExistingNicknames();
+  }, []);
 
-        // Assuming user object contains ID
-        if (user.google_id) {
-            fetchUserData();
-        }
-    }, [user.google_id]);
+  const fetchUserData = async () => {
+    try {
+      const userData = await apiget(`user/${user.userid}`);
+      if (userData) {
+        setVorname(userData.vorname || '');
+        setNachname(userData.nachname || '');
+        setNickname(userData.nickname || '');
+        setIsEditMode(true);
+        console.log("userDAta",userData)
+      }
+    } catch (error) {
+      console.error('Fehler beim Laden des Profils:', error);
+    }
+  };
 
-    const handleSaveProfile = async () => {
-        try {
-            if (isEditMode) {
-                // Update existing profile
-                await apiput(`users/${user.google_id}`, {
-                    vorname: vorname,
-                    nachname: nachname,
-                    nickname: nickname,
-                    // Add other profile data here
-                });
-                console.log("Profil erfolgreich aktualisiert");
-            } else {
-                // Create new profile
-                await apiput('users', {
-                    vorname: vorname,
-                    nachname: nachname,
-                    nickname: nickname,
-                    // Add other profile data here
-                });
-                console.log("Profil erfolgreich erstellt");
-            }
-        } catch (error) {
-            console.error("Fehler beim Speichern des Profils:", error);
-        }
-    };
+  const fetchExistingNicknames = async () => {
+    try {
+      const nicknamesData = await apiget('users/nicknames');
+      if (nicknamesData) {
+        setExistingNicknames(nicknamesData.nicknames || []);
+      }
+    } catch (error) {
+      console.error('Fehler beim Laden der Nicknames:', error);
+    }
+  };
 
-    const handleDeleteProfile = async () => {
-        try {
-            // Assuming google_id contains the ID of the profile to be deleted
-            const confirmDelete = window.confirm("Möchten Sie Ihr Profil wirklich löschen?");
-            
-            if (confirmDelete) {
-                await apidelete(`users/${user.google_id}`);
-                console.log("Profil erfolgreich gelöscht");
-                // Optionally, you can add logic to navigate to another page or perform other actions after deletion
-            } else {
-                console.log("Löschen des Profils abgebrochen");
-            }
-        } catch (error) {
-            console.error("Fehler beim Löschen des Profils:", error);
-        }
-    };
+  const handleSaveProfile = async () => {
+    try {
+      if (isEditMode) {
+        // Update existing profile
+        await apiput('users', user.google_id, {
+          vorname: vorname,
+          nachname: nachname,
+          nickname: nickname,
+          // Add other profile data here
+        });
+        console.log('Profil erfolgreich aktualisiert');
+        // Re-fetch user data after updating
+        fetchUserData();
+      } else {
+        // Create new profile
+        const existingUser = await apipost('users', {
+          id: 0,
+          nachname: nachname,
+          vorname: vorname,
+          nickname: nickname,
+          google_id: user?.userid,
+        });
 
-    return (
-        <>
-            <Box sx={{ marginTop: "8rem" }}>
-                <Card sx={{ marginLeft: "auto", marginRight: "auto", width: "20rem", padding: "2rem" }}>
-                    <Typography align="center" variant='h5' mb={"1rem"}>Persönliche Daten</Typography>
-                    <TextField style={input} label="Vorname" id="textinput" value={vorname} onChange={(e) => setVorname(e.target.value)} />
-                    <TextField style={input} label="Nachname" id="textinput" value={nachname} onChange={(e) => setNachname(e.target.value)} />
-                    <TextField style={input} label="Nickname" id="textinput" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-                    <Button color="success" sx={{ marginLeft: "1rem" }} onClick={handleSaveProfile}>
-                        {isEditMode ? 'Aktualisieren' : 'Erstellen'}
-                    </Button>
-                    <Button color='error' sx={{ float: "right", marginRight: "1rem" }} onClick={handleDeleteProfile}>
-                        Profil löschen
-                    </Button>
-                </Card>
-            </Box>
-        </>
-    );
+        await setUser({ ...user, ['id']: existingUser.id });
+
+        console.log('Profil erfolgreich erstellt');
+        // Re-fetch user data after creating
+        fetchUserData();
+      }
+    } catch (error) {
+      console.error('Fehler beim Speichern des Profils:', error);
+    }
+  };
+
+  const handleDeleteProfile = async () => {
+    try {
+      const confirmDelete = window.confirm('Möchten Sie Ihr Profil wirklich löschen?');
+
+      if (confirmDelete) {
+        await apidelete('users', user.google_id);
+        console.log('Profil erfolgreich gelöscht');
+        // Optionally, you can add logic to navigate to another page or perform other actions after deletion
+      } else {
+        console.log('Löschen des Profils abgebrochen');
+      }
+    } catch (error) {
+      console.error('Fehler beim Löschen des Profils:', error);
+    }
+  };
+
+  const handleEditProfile = () => {
+    setIsEditMode(true);
+  };
+
+  return (
+    <>
+      <Box sx={{ marginTop: '8rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Card sx={{ width: '30rem' }}>
+          <Box sx={{ padding: '2rem' }}>
+            <Typography align="center" variant="h5" mb="1rem">
+              {isEditMode ? 'Profil bearbeiten' : 'Profil anzeigen'}
+            </Typography>
+            <TextField
+              required
+              style={input}
+              label="Vorname"
+              id="textinput"
+              value={vorname}
+              disabled={!isEditMode}
+            />
+            <TextField
+              required
+              style={input}
+              label="Nachname"
+              id="textinput"
+              value={nachname}
+              disabled={!isEditMode}
+            />
+            <TextField
+              required
+              style={input}
+              label="Nickname"
+              id="textinput"
+              helperText={helper}
+              value={nickname}
+              disabled={!isEditMode}
+            />
+          </Box>
+          <Box mb="1rem" sx={{ display: 'flex', justifyContent: 'center' }}>
+            {!isEditMode && (
+              <Button
+                sx={{ outline: '1px solid blue', color: 'blue', marginRight: '1rem' }}
+                onClick={handleEditProfile}
+              >
+                Profil speichern
+              </Button>
+            )}
+            <Button
+              sx={{ outline: '1px solid green', color: 'green', marginRight: '1rem' }}
+              onClick={handleSaveProfile}
+              endIcon={<SendIcon />}
+              disabled={!isEditMode || !vorname || !nachname || !nickname}
+            >
+              Profil {isEditMode ? 'aktualisieren' : 'erstellen'}
+            </Button>
+            {isEditMode && (
+              <Button
+                sx={{ outline: '1px solid red', color: 'red' }}
+                onClick={handleDeleteProfile}
+                startIcon={<DeleteIcon />}
+              >
+                Profil löschen
+              </Button>
+            )}
+          </Box>
+        </Card>
+      </Box>
+    </>
+  );
 }
+
+const input = {
+  marginTop: '1rem',
+  backgroundColor: 'white',
+  width: '100%',
+};
